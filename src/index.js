770a549e407aa5fbb004bb956d48e46cd26267ed
Irona26 <irina_kulinich@mail.ru>
2022-03-02 22:28:27 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+let n = 514;
+module.exports = function reversee (n) {
+    return (
+        parseFloat(
+          n
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        )
+      )
+    }
+    console.log(n);
